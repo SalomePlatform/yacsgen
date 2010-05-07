b4903bb12071a3d18b87d98b01ca919d2afeaa56
vsr <vsr@opencascade.com>
2010-05-07 15:58:59 +0000

    Merge from V5_1_4_BR 07/05/2010

--- Examples/ast1/fcompo/compo.f
+++ Examples/ast1/fcompo/compo.f
@@ -1,3 +1,22 @@
+C  Copyright (C) 2009-2010  EDF R&D
+C
+C  This library is free software; you can redistribute it and/or
+C  modify it under the terms of the GNU Lesser General Public
+C  License as published by the Free Software Foundation; either
+C  version 2.1 of the License.
+C
+C  This library is distributed in the hope that it will be useful,
+C  but WITHOUT ANY WARRANTY; without even the implied warranty of
+C  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+C  Lesser General Public License for more details.
+C
+C  You should have received a copy of the GNU Lesser General Public
+C  License along with this library; if not, write to the Free Software
+C  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+C
+C  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+C
+
        SUBROUTINE S1(compo,A,B,C,D,E,F)
        include 'calcium.hf'
        integer compo
--- module_generator/pacocompo.py
+++ module_generator/pacocompo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 *-
-#  Copyright (C) 2009 - EDF R&D
+#  Copyright (C) 2009-2010  EDF R&D
 #
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
@@ -16,6 +16,9 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #
+#  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#
+
 #  Author : Andre RIBES (EDF R&D)
 
 """
