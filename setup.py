f215d8173b8ffadc5e88ab1134673ece8096d905
inv <inv@opencascade.com>
2011-06-21 11:43:35 +0000

    Changing version to 6.3.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@
 
 from distutils.core import setup
 setup(name='YACSGEN',
-      version='6.3.0',
+      version='6.3.1',
       author='C. Caremoli',
       packages=['module_generator'],
      )
