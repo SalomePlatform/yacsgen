37b773ca165662142b1d12502e0b1a12ca0ddf5a
vsr <vsr@opencascade.com>
2015-04-17 18:47:34 +0300

    Increment version: 7.6.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@
 
 from distutils.core import setup
 setup(name='YACSGEN',
-      version='7.5.1',
+      version='7.6.0',
       author='C. Caremoli',
       packages=['module_generator'],
       scripts=['script/hxx2salome.py',]
