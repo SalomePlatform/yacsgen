568cd78a8b974045bb793a83049bc1b38924e7a2
mbs <martin.bernhard@opencascade.com>
2024-01-20 14:20:19 +0000

    Updated copyright comment

--- script/hxx2salome.py
+++ script/hxx2salome.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 #  -*- coding: iso-8859-1 -*-
-# Copyright (C) 2009-2023  EDF
+# Copyright (C) 2009-2024  EDF
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
